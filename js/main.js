3e9bbd0a68a3786aa00c87a72b628c3c58ad0c16
Juan Fernando Jaramillo <jfjaramillo4@espe.edu.ec>
2025-11-08 19:47:35 -0500

    .

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$('.carousel').carousel({
-  interval: 3000,
-  pause: "hover"
-});
-try {
-  if ($.fn && $.fn.carousel) {
-    $('.carousel').carousel({ interval: 3000, pause: 'hover' });
-  }
-} catch(e) {}
